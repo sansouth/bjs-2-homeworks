22e0a2d05da17a4d20f9b0e748c1eff06d98ddf5
san_south <alexandrershov22@mail.ru>
2023-01-28 16:04:28 +0300

    первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,28 +1,46 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let D = b**2-4*a*c
-  let x1, x2;
+  let rootFirst;
+  // let rootSecond;
+  let d = Math.pow(b, 2) - 4 * a * c;
 
-  if (D < 0) {
-    console.log("корней нет")
-  }
-  else if (D === 0) [
-    x1 = -b/(2*a)
-    console.log("один корень")
-  ]
-  else if (D > 0) {
-    x1 = (-b + Math.sqrt(D) )/(2*a);
-    x2 = (-b - Math.sqrt(D) )/(2*a);
-    console.log("два корня")
-  }
+    if (d === 0) {
+      rootFirst = -b / (2 * a);
+      arr.push(rootFirst);
+      console.log(arr);
+    } else if (d > 0) {
+      rootFirst = (-b + Math.sqrt(d) ) / (2 * a);
+      let rootSecond = (-b - Math.sqrt(d) ) / (2 * a);
+      arr.push(rootFirst);
+      arr.push(rootSecond);
+      console.log('Первый корень = ' + arr[0] + 'Второй корень = ' + arr[1]);
+    } else {
+      console.log('Корней нет');
+    }
+
   return arr;
 }
 
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  Let P = (percent / 100) / 12;
-  Let S = amount - contribution;
-  Let n = countMonths;
-  Let payment = S * (P + (P / (((1 + P)^n) - 1)))
+  let bodyCredit;
+  let monthPayment;
+  let totalAmount;
+
+  if (
+    isNaN(percent) ||
+    isNaN(contribution) ||
+    isNaN(amount) ||
+    isNaN(countMonths) 
+  ) {
+      // console.log('Один из параметров сожержит неверное значение');
+      return false;
+    }
+
+    percent = percent / 100 / 12;
+    bodyCredit = amount - contribution;
+    monthPayment = bodyCredit * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+    totalAmount = parseFloat((monthPayment * countMonths).toFixed(2));
+
+    return totalAmount;
 }
