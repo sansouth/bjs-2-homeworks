35762082bc9d141d988c4d43a4f1f950bf904545
san_south <alexandrershov22@mail.ru>
2023-01-28 18:27:52 +0300

    второе задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,83 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    sum += arr[i];
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+
+  return { min: min, max: max, avg: avg };  
 }
-
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  // let max = arr[0];
+  // let min = arr[0];
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] > max) {
+  //     max = arr[i];
+  //   } else if (arr[i] < min) {
+  //     min = arr[i];
+  //   }
+  // }
+  // let difference = max - min;
+  // return difference;
 
+  if (arr.length >= 0) {
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    return(max-min);
+  }
 }
 
 function differenceEvenOddWorker(...arr) {
-
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement += arr[i];
+      } else if (arr[i] % 2 != 0) {
+        sumOddElement += arr[i];
+      }
+    }
+    return(sumEvenElement - sumOddElement);
 }
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    }
+  }
+  return(sumEvenElement / countEvenElement);
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = -Infinity;
+ for (let i = 0; i < arrOfArr.length; i++) {
+    const resultFunc = func(...arrOfArr[i]);
+    if (resultFunc > maxWorkerResult) {
+      maxWorkerResult = resultFunc;
+    }
+  }
+ return maxWorkerResult;
 }
