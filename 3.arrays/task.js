5427a3ef8e74b106aec65fe693df0453f821d2e1
san_south <alexandrershov22@mail.ru>
2023-01-29 17:58:09 +0300

    третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(users=> users.gender === gender).map(users=> users.age).reduce((acc, item, index, arr)=> {
+        acc += item;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc; 
+    }, 0) 
+    return result;
 }
