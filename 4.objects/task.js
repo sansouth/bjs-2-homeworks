294a17531821461ed0f0d204470e3444a43668e5
san_south <alexandrershov22@mail.ru>
2023-02-11 15:20:45 +0300

    задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marks) {
+    if (this.marks !== undefined) {
+      this.marks.push(...marks);
+    } else {
+      console.log ('Студент отчислен');
+    }
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+    let sum = 0;
+    let count = 0;
+    let result;
+    if (this.marks !== undefined && this.marks.length > 0) {
+      for (let mark of this.marks) {
+        sum += mark;
+        count += 1;
+      }
+      result = sum / count;
+    } else {
+      result = 0;
+    }
+    return result;
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+  }
