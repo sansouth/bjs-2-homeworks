636384792ff6215ac8b9a01832fe29325fcfe74c
san_south <alexandrershov22@mail.ru>
2023-03-11 17:16:30 +0300

    Выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿// Task 1
+
+function parseCount(count) {
+    const result = Number.parseFloat(count);
+    if(!result) {
+        throw new Error("Невалидное значение");
+    } 
+    return result;
+}
+function validateCount(count) {
+    try {
+        return parseCount(count);
+    } catch(error) {
+        return error;
+    }
+}
+
+// Task 2
+
+class Triangle {
+    constructor(a, b, c) {
+        if((a + b) < c || (a + c) < b || (b + c) < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+    get area() {
+        const pp = this.perimeter / 2;
+        return parseFloat(Math.sqrt((pp * (pp - this.a) * (pp - this.b) * (pp - this.c))).toFixed(3));
+    }
+}
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+
+    } catch(error) {
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует"
+            },
+            get area() {
+                return "Ошибка! Треугольник не существует"
+            }
+        }
+    }
+}
